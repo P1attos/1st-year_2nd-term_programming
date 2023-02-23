52ba5d381932eed09143f4f01865aeb8d8b4143f
EIABELS <111728034+P1attos@users.noreply.github.com>
2023-02-23 16:29:36 +0800

    Update tomie.java

--- code/tomie.java
+++ code/tomie.java
@@ -4,17 +4,23 @@ import java.awt.*;
 public class tomie {
     JFrame frame = new JFrame();
     ImageIcon bg = new ImageIcon("tomie.jpg");
+    JButton btn = new JButton("Login");
     
 
     public tomie (){
         Image img = bg.getImage();
-        bg = new ImageIcon(img.getScaledInstance(500, 500, Image.SCALE_SMOOTH));
-        frame.setSize(500,500);
+        bg = new ImageIcon(img.getScaledInstance(500, 800, Image.SCALE_SMOOTH));
+        frame.setSize(500,800);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 
         JLabel lblBack = new JLabel(bg);
         lblBack.setLayout(new FlowLayout());
-        lblBack.setBounds(0, 0, 500, 500);
+        lblBack.setBounds(0, 0, 500, 800);
+        
+        btn.setBounds(100, 100, 100, 100);
+        lblBack.add(btn);
+
+
         frame.add(lblBack);
 
         frame.setVisible(true);
